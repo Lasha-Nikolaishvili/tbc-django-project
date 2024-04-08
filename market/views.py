7127eb1dd38568ef88a8273472a6515af666979a
Lasha Nikolaishvili <nikolaishvililashan1@gmail.com>
2024-04-08 18:14:40 +0400

    fixing author endpoints

--- market/views.py
+++ market/views.py
@@ -5,6 +5,7 @@ from django.core.serializers import serialize
 from django.core.serializers.base import Serializer
 from django.core.exceptions import ObjectDoesNotExist
 from market.custom_book_serializer import CustomBookSerializer
+import json
 
 
 def get_books(request):
@@ -13,13 +14,12 @@ def get_books(request):
         books, ensure_ascii=False, fields=['name', 'author', 'category', 'page_count', 'price']
     )
 
-    return HttpResponse(books_json, content_type='application/json', status=200)
+    return HttpResponse(books_json,  content_type='application/json', status=200)
 
 
 def get_book(request, book_id):
     try:
         book = Book.objects.get(pk=book_id)
-        print(book)
         book_item = {
             'name': book.name,
             'author': f'{book.author.first_name} {book.author.last_name}',
@@ -34,9 +34,8 @@ def get_book(request, book_id):
 
 
 def get_authors(request):
-    authors = Author.objects.all()
-    print(authors)
-    authors_json = serialize('json', authors)
+    authors = Author.objects.only('first_name', 'last_name', 'nationality', 'age')
+    authors_json = serialize('json', authors, fields=['first_name', 'last_name', 'nationality', 'age'])
 
     return HttpResponse(authors_json, content_type='application/json', status=200)
 
@@ -44,12 +43,15 @@ def get_authors(request):
 def get_author(request, author_id):
     try:
         author = Author.objects.get(pk=author_id)
-        print(author)
+        books = author.book_set.only('name')
+        books_data = serialize('json', books, fields=['name'])
+
         author_item = {
             'first_name': author.first_name,
             'last_name': author.last_name,
             'nationality': author.nationality,
             'age': author.age,
+            'books': json.loads(books_data)
         }
 
         return JsonResponse(author_item, status=200)
